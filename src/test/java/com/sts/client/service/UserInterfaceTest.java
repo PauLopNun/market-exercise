3e59327f80e7e141db038982d324205398a05d83
mion <913mario@gmail.com>
2026-03-23 16:52:57 +0100

    Tests

--- src/test/java/com/sts/client/service/UserInterfaceTest.java
+++ src/test/java/com/sts/client/service/UserInterfaceTest.java
@@ -1,12 +1,17 @@
 package com.sts.client.service;
 
+import com.sts.shared.model.CSVReader;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserInterfaceTest {
     @Test
-    void testValidInput(){
+    void testValidInput() {
 
     }
 }
